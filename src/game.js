e6928bfd1dcb2f8e770c6238aa7606cb190c00db
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-03-12 16:37:23 +0000

    Move allows someone to place an X in the field

--- src/game.js
+++ src/game.js
@@ -1,5 +1,9 @@
 class Game {
   fields = ['', '', '', '', '', '', '', '', '']
+
+  move(place){
+    this.fields[place - 1] = 'X'
+  }
 }
 
 module.exports = Game
--- test/game.unit.test.js
+++ test/game.unit.test.js
@@ -1,9 +1,19 @@
 const Game = require('../src/game')
 
+let game;
+
+beforeEach(() => {
+  game = new Game()
+})
+
 describe('Game', () => {
   test('A new game sets up with an array of 9 empty strings', () => {
-    let game = new Game()
     expect(game.fields.length).toEqual(9)
     expect(game.fields).toEqual(['', '', '', '', '', '', '', '', ''])
+  })
+
+  test('A user can make a move', () => {
+    game.move(4)
+    expect(game.fields).toEqual(['', '', '', 'X', '', '', '', '', ''])
   })
 })
